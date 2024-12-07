cafddec8f25c7740a2cedd7cbff7a618039ca51d
dratu <RWTreutle@gmail.com>
2024-12-07 13:05:55 +0100

    directory's in the "root" directory will now be ignored

--- src/musicPlayer/WavFilter.java
+++ src/musicPlayer/WavFilter.java
@@ -6,6 +6,8 @@ import java.io.FilenameFilter;
 public class WavFilter implements FilenameFilter {
     @Override
     public boolean accept(File dir, String name) {
+        if (new File(dir, name).isDirectory())
+            return false;
         return name.endsWith(".wav");
     }
 }
