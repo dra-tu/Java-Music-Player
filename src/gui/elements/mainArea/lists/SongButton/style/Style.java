fb12608641cd2bfc15157e9277ce5e95a46d0a15
DraTu <RWTreutle@gmail.com>
2025-04-08 21:06:06 +0200

    deleted unused interface

--- src/gui/elements/mainArea/lists/SongButton/style/Style.java
+++
@@ -1,8 +0,0 @@
-package gui.elements.mainArea.lists.SongButton.style;
-
-import javax.swing.*;
-
-public interface Style {
-    public<C extends JComponent> void applyStyle(C component);
-    public<C extends JComponent> void removeStyle(C component);
-}
