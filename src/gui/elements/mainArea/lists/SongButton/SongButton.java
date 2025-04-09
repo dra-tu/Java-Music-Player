d49bcd1c4041685b4c50a93ae56339fdf8e56871
DraTu <RWTreutle@gmail.com>
2025-04-09 06:48:18 +0200

    collapsed SongButtonController in the SongButton class

--- src/gui/elements/mainArea/lists/SongButton/SongButton.java
+++ src/gui/elements/mainArea/lists/SongButton/SongButton.java
@@ -4,13 +4,23 @@ import musicPlayer.MusicPlayer;
 import musicPlayer.songTypes.Song;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SongButton extends JButton {
+public class SongButton extends JButton implements ActionListener {
+    private final MusicPlayer musicPlayer;
+    private final int SONG_ID;
+
     public SongButton(Song song, MusicPlayer musicPlayer) {
-        super(String.format("%2d: %s", song.SONG_ID + 1, song.getName()));
+        super(String.format("%3d: %s", song.SONG_ID + 1, song.getName()));
+        this.musicPlayer = musicPlayer;
+        this.SONG_ID = song.SONG_ID;
 
+        this.addActionListener(this);
+    }
 
-
-        new SongButtonController(this, musicPlayer, song.SONG_ID);
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        musicPlayer.startSong(SONG_ID);
     }
 }
