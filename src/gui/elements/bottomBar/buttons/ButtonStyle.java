f57f5a4df26b9efce610d900b98718672277fbde
DraTu <RWTreutle@gmail.com>
2025-04-12 06:15:57 +0200

    removed implements JmpColored from Button Style

--- src/gui/elements/bottomBar/buttons/ButtonStyle.java
+++ src/gui/elements/bottomBar/buttons/ButtonStyle.java
@@ -1,23 +1,14 @@
 package gui.elements.bottomBar.buttons;
 
-import gui.color.JmpColored;
-import gui.color.JmpGuiColorPalette;
-
 import javax.swing.*;
 import java.awt.*;
 
-public class ButtonStyle implements JmpColored {
+public class ButtonStyle {
     public ButtonStyle(JButton button, String text) {
-        Dimension size = new Dimension(150, 50);
+         Dimension size = new Dimension(150, 50);
 
         button.setPreferredSize(size);
 
         button.setText(text);
-    }
-
-
-    @Override
-    public <CP extends JmpGuiColorPalette> void updateColors(CP cp) {
-
     }
 }
